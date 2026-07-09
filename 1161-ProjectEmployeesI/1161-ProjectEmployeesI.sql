8f3c297956bd509d68017417dd0fe8b81212839d
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:07:27 +0530

    Create 1161-ProjectEmployeesI

---
+++ 1161-ProjectEmployeesI/1161-ProjectEmployeesI.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/9/2026, 3:07:27 PM
+
+SELECT p.project_id, ROUND(AVG(e.experience_years),2) AS average_years
+FROM Project p 
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
