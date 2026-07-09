cc631fd78f8ffcee0936b42dfd2cbfb34b0d22f9
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:11:54 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:11:55 PM
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int k = 0;
+        for(int i = 1;i<intervals.length;i++){
+            if(intervals[k][1] >= intervals[i][0]){
+                intervals[k][1] = Math.max(intervals[i][1],intervals[k][1]);
+                }else{
+                    k++;
+                    intervals[k] = intervals[i];
+                }
+            }
+            return Arrays.copyOfRange(intervals,0,k+1);
+        }
+    }
