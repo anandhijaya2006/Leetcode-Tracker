50ff6420fffa76ae3d53596a96283767d3c4df19
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:07:33 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 3:07:34 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0)
+        return true;
+        else
+        return false;
+        
+    }
+};
