fd27c960153c8fd1bdc2184dcfb642fcac14d71e
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:09:03 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 3:09:03 PM
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4!=0){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
