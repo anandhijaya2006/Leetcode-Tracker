6f5a75f7d4ae63226c25625ae7e699401f4d2c94
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:07:44 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 3:07:45 PM
+class Solution {
+    public int maxi(int[] piles) {
+        int m = 0;
+        for(int i:piles) {m = Math.max(m,i);}
+        return m;
+    }
+    public int calculate(int[] piles, int mid){
+        int c = 0;
+        for(int i : piles){
+            c += (i/mid);
+            if(i%mid != 0) c++;
+        }
+        return c;
+    }
+    public int minEatingSpeed(int[] piles, int h){
+        int s = 1;
+        int e = maxi(piles);
+        while(s < e){
+            int mid = (s+e)/2;
+            int cal_hours = calculate(piles,mid);
+            if(cal_hours > h) s = mid+1;
+            else e = mid;
+        }
+        return s;
+    }
+}
