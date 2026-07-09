894bd7d892b6b7f7c1516ffb87f2a1723309bfde
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:06:18 +0530

    Create 2748-CalculateDelayedArrivalTime

---
+++ 2748-CalculateDelayedArrivalTime/2748-CalculateDelayedArrivalTime.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:06:18 PM
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        if((arrivalTime+delayedTime) <24){
+            return arrivalTime+delayedTime;
+        }else{
+            return 
+           ( arrivalTime+delayedTime)%24;
+        }
+        
+    }
+};
