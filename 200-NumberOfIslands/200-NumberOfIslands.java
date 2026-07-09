cc8ab1acd66e37acfb9c0ef7ceaf5324a08894b6
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:09:43 +0530

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 3:09:44 PM
+class Solution {
+    void travel(char[][] grid,int row,int col){
+            if(col>=grid[0].length || col<0 || row<0 || row >= grid.length || grid[row][col] == '0') return;
+            grid[row][col] = '0';
+            travel(grid,row,col+1);
+            travel(grid,row,col-1);
+            travel(grid,row-1,col);
+            travel(grid,row+1,col);
+        }
+    public int numIslands(char[][] grid) {
+        int c=0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == '1'){
+                    c++;
+                    travel(grid,i,j);
+                }
+            }
+        }
+        return c;
+        
+    }
+}
