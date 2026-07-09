2d4548543193159e4a63cb75fcc1754fdb61e832
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:05:21 +0530

    Create 4342-CreateGridWithExactlyOnePath

---
+++ 4342-CreateGridWithExactlyOnePath/4342-CreateGridWithExactlyOnePath.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:05:22 PM
+class Solution {
+    public String[] createGrid(int m, int n) {
+        String[] grid = new String[m];
+
+        for(int i=0;i<m;i++){
+            StringBuilder row = new StringBuilder();
+            for(int j=0;j<n;j++){
+                if(i==0 || j==n-1){
+                    row.append('.');
+                }else{
+                    row.append('#');
+                }
+            }
+            grid[i] = row.toString();
+        }
+        return grid;
+    }
+}
