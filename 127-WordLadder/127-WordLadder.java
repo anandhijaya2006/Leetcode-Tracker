ecf9c8baa7813e05d73f41ec7237d6d1071dbfc0
anandhijaya2006 <anandhijaya2006@gmail.com>
2026-07-09 15:10:38 +0530

    Create 127-WordLadder

---
+++ 127-WordLadder/127-WordLadder.java
@@ -0,0 +1,53 @@
+// Last updated: 7/9/2026, 3:10:39 PM
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+
+       
+        if (!wordSet.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> wordQueue = new LinkedList<>();
+        wordQueue.add(beginWord);
+        int distance = 1;
+
+       
+        while (!wordQueue.isEmpty()) {
+            int size = wordQueue.size();
+
+            
+            for (int i = 0; i < size; i++) {
+                String currWord = wordQueue.poll();
+
+                
+                if (currWord.equals(endWord)) {
+                    return distance;
+                }
+
+             
+                for (int j = 0; j < currWord.length(); j++) {
+                    char[] temp = currWord.toCharArray();
+
+                    
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        temp[j] = c;
+                        String newWord = new String(temp);
+
+                        
+                        if (wordSet.contains(newWord)) {
+                            wordQueue.add(newWord);
+                            wordSet.remove(newWord); 
+                        }
+                    }
+                }
+            }
+
+            
+            distance++;
+        }
+
+        
+        return 0;
+    }
+}
